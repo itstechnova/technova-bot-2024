439fdaef96e4071e09707145a9185f00ebdcf359
Prachee Nanda <p4nanda@uwaterloo.ca>
2024-07-06 10:15:30 -0400

    add hello command to greet users

--- bot.py
+++ bot.py
@@ -1,30 +1,39 @@
 import os
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+# Set up intents
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True  # Enable the message content intent
 
-intents = discord.Intents(8)
-client = discord.Client(intents=intents)
+# Define the bot command prefix and intents
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == GUILD:
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})\n'
     )
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-client.run(TOKEN)
+# Example command
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send(f'Hello, {ctx.author.name}!')
+
+# Run the bot
+bot.run(TOKEN)
